db7eb41441f5f69e98ef0386b5391d1584626bd1
Iosif Oprea <oprea.iosif002@gmail.com>
2023-02-01 00:01:59 +0200

    router login error fixed

--- frontend/src/router/auth-routes.ts
+++ frontend/src/router/auth-routes.ts
@@ -2,18 +2,18 @@ export default
 {
     path: "/",
     name: "auth",
-    component: import("@/views/Layouts/AuthLayout.vue"),
+    component: () => import("@/views/Layouts/AuthLayout.vue"),
     redirect: { name: 'login' },
     children: [
-    {
-        path: "/login",
-        name: "login",
-        component: () => import("@/views/Auth/LoginView.vue"),
-    },
-    {
-        path: "/register",
-        name: "register",
-        component: () => import("@/views/Auth/RegisterView.vue"),
-    },
+        {
+            path: "/login",
+            name: "login",
+            component: () => import("@/views/Auth/LoginView.vue"),
+        },
+        {
+            path: "/register",
+            name: "register",
+            component: () => import("@/views/Auth/RegisterView.vue"),
+        },
     ]
 };
