b1fbe9c81c1b4d736c7b09b0af70c2b7095c585e
Octavian <octavianoprea03@gmail.com>
2023-04-20 16:26:47 +0300

    books page functionality

--- frontend/src/views/Pages/Books/BooksView.vue
+++ frontend/src/views/Pages/Books/BooksView.vue
@@ -1,10 +1,29 @@
 <script setup lang="ts">
+import PopularBooks from '@/components/books/PopularBooksComponent.vue';
+import Pagination from '@/components/global/PaginationComponent.vue';
+import { useBooksStore } from '@/stores/books-store';
+
+const store = useBooksStore();
+store.searchBooks();
+
 </script>
 
 <template>
-	<div>
-    Books
-	</div>
+	<Loading v-if="store.isLoading" />
+
+  <SearchBar
+    :defaultValue="store.books.searchModel.title"
+    @valueChanged="(value: any) => (store.books.searchModel.title = value, store.books.searchModel.pagination.page = 0, store.searchBooks())"
+    placeholder='Search book...'
+  />
+
+  <PopularBooks :books="store.books.data" />
+
+  <Pagination
+    :current-page="store.books.searchModel.pagination.page"
+    :last-page="store.books.searchModel.pagination.last_page"
+    @change-page="(page: number) => (store.books.searchModel.pagination.page = page, store.searchBooks())"
+  />
 </template>
 
 <style scoped>
